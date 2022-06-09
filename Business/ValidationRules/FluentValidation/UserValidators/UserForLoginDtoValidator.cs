d6679d01e5fafc33ab7a38da627c0fc09f11193b
Adem Kınataş <ademkinatas@gmail.com>
2022-06-10 00:30:35 +0300

    added user validators

---
+++ Business/ValidationRules/FluentValidation/UserValidators/UserForLoginDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation.UserValidators
+{
+    public class UserForLoginDtoValidator : AbstractValidator<UserForLoginDto>
+    {
+        public UserForLoginDtoValidator()
+        {
+            RuleFor(p => p.Email).NotEmpty().WithMessage("Mail adresi boş olamaz");
+            RuleFor(p => p.Email).EmailAddress().WithMessage("Geçerli bir mail adresi yazın");
+            RuleFor(p => p.Password).NotEmpty().WithMessage("Şifre boş olamaz");
+            RuleFor(p => p.Password).MinimumLength(6).WithMessage("Şifre en az 6 karakter olmalıdır");
+            RuleFor(p => p.Password).Matches("[A-Z]").WithMessage("Şifreniz en az 1 adet büyük harf içermelidir");
+            RuleFor(p => p.Password).Matches("[a-z]").WithMessage("Şifreniz en az 1 adet küçük harf içermelidir");
+            RuleFor(p => p.Password).Matches("[0-9]").WithMessage("Şifreniz en az 1 adet sayı içermelidir");
+            RuleFor(p => p.Password).Matches("[^a-zA-Z0-9]").WithMessage("Şifreniz en az 1 adet özel karakter içermelidir");
+        }
+    }
+}
